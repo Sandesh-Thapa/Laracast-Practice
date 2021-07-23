858ec4e1bf3eb7fa2470208b0e73df7ef593be62
Sandesh-Thapa <sandeshthapa426@gmail.com>
2021-07-23 13:35:54 +0545

    Completed Section 6

--- app/Post.php
+++ app/Post.php
@@ -3,14 +3,26 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-//use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
-    //use HasFactory;
     protected $guarded = [];
 
     protected $with = ['category', 'author'];
+
+    public function scopeFilter($query, array $filters){
+        // if($filters['search'] ?? false){
+        //     $query
+        //         ->where('title', 'like', '%' .request('search'). '%')
+        //         ->orWhere('body', 'like', '%' .request('search'). '%');
+        // }
+
+        $query->when($filters['search'] ?? false, function ($query, $search) {
+            $query
+                ->where('title', 'like', '%' .$search. '%')
+                ->orWhere('body', 'like', '%' .$search. '%');
+        });
+    }
     
     public function category(){
         return $this->belongsTo(Category::class);
@@ -19,7 +31,4 @@ class Post extends Model
     public function author(){
         return $this->belongsTo(User::class, 'user_id');
     }
-    // public function user(){
-    //     return $this->belongsTo(User::class);
-    // }
 }
